fac4e250981de330f682538cb3c944d228ccbc67
Nourhan Sayed <58532294+nour1698@users.noreply.github.com>
2022-08-13 10:08:22 +0200

    dashboard

---
+++ src/components/navBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <div class="header">
+        hello
+      </div>
+    </div>
+</template>
+
+<style scoped>
+.header{
+    position: fixed;
+    top:0;
+}
+</style>
---
+++ src/views/homePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <navBar>
+
+        </navBar>
+<p>nnn</p>
+    </div>
+</template>
+<script>
+import navBar from "../components/navBar.vue"
+export default {
+    components:{
+        navBar 
+        
+    }
+}
+</script>
